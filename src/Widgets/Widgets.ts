48fbe6ed7a7b177d0cb9692a2f294e84f1967f00
Kyle Derkacz <kyle.derkacz@boulder.care>
2019-07-22 16:00:23 -0700

    Removing range widget to be compatible with previous versions of material-ui

--- src/Widgets/Widgets.ts
+++ src/Widgets/Widgets.ts
@@ -2,7 +2,6 @@ import CheckboxWidget from '../CheckboxWidget/CheckboxWidget';
 import CheckboxesWidget from '../CheckboxesWidget/CheckboxesWidget';
 import PasswordWidget from '../PasswordWidget/PasswordWidget';
 import RadioWidget from '../RadioWidget/RadioWidget';
-import RangeWidget from '../RangeWidget/RangeWidget';
 import SelectWidget from '../SelectWidget/SelectWidget';
 import TextareaWidget from '../TextareaWidget/TextareaWidget';
 import TextWidget from '../TextWidget/TextWidget';
@@ -13,7 +12,6 @@ export default {
   CheckboxesWidget,
   PasswordWidget,
   RadioWidget,
-  RangeWidget,
   SelectWidget,
   TextareaWidget,
   TextWidget,
